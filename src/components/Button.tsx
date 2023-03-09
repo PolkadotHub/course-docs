483a3b42e45835f28b9d341886663a4799f8a50d
Francisco Aguirre <franciscoaguirreperez@gmail.com>
2023-03-09 18:05:36 -0300

    Starting work on registration form

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,13 +4,22 @@ import clsx from 'clsx';
 
 export interface ButtonProps {
   onClick: () => void;
-  variant?: 'green' | 'purple';
+  variant?: 'orange' | 'green' | 'purple';
   children?: JSX.Element;
+}
+
+const getBackgroundColorClass = (variant: NonNullable<ButtonProps['variant']>) => {
+  if (variant === 'orange') {
+    return 'bg-orange';
+  } else if (variant === 'green') {
+    return 'bg-green';
+  }
+  return 'bg-purple';
 }
 
 export const Button: Component<ButtonProps> = (props) => {
   const merged = mergeProps({ variant: 'green' }, props);
-  const backgroundColorClass = merged.variant === 'green' ? 'bg-green' : 'bg-purple';
+  const backgroundColorClass = getBackgroundColorClass(merged.variant);
   
   return (
     <button onClick={merged.onClick} class={clsx('rounded px-5 py-3', backgroundColorClass)}>
