1cbbd9baec16d4d3bdee31966094d607951cfadc
Brechy <brechy.web3@gmail.com>
2023-03-23 10:33:38 -0300

    fix: index

--- src/data/navItems.ts
+++ src/data/navItems.ts
@@ -1,12 +1,16 @@
 export const navItems = [
+  {
+    href: '/',
+    text: 'Home',
+  },
   // {
   //   href: '/rust',
   //   text: 'Rust',
   // },
-  {
-    href: "/substrate",
-    text: "Substrate",
-  },
+  // {
+  //   href: "/substrate",
+  //   text: "Substrate",
+  // },
   // {
   //   href: '/ink',
   //   text: 'Ink',
